07762b4ffa80e51ea425caf6907545b431e4aebd
KY <249657796+keewai704@users.noreply.github.com>
2026-09-05 19:34:59 +0900

    fix(citrus): make active windows fully opaque

--- hosts/citrus/hyprland.lua
+++ hosts/citrus/hyprland.lua
@@ -57,7 +57,7 @@ hl.config({
     decoration = {
         rounding = 12,
         rounding_power = 2,
-        active_opacity = 0.99,
+        active_opacity = 1.0,
         inactive_opacity = 0.96,
         fullscreen_opacity = 1.0,
         dim_inactive = true,
