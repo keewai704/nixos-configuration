331e4b8611911cbffc5632e30c908daae2cb050e
KY <249657796+keewai704@users.noreply.github.com>
2026-09-15 12:43:34 +0900

    fix(home): preserve GitHub CLI settings on update

--- home/keewai/common.nix
+++ home/keewai/common.nix
@@ -2,7 +2,25 @@
 {
   imports = [ ./shared ];
 
-  programs.gh.enable = true;
+  programs.gh = {
+    enable = true;
+    settings = {
+      accessible_colors = "disabled";
+      accessible_prompter = "disabled";
+      aliases.co = "pr checkout";
+      browser = "";
+      color_labels = "disabled";
+      editor = "";
+      git_protocol = "https";
+      http_unix_socket = "";
+      pager = "";
+      prefer_editor_prompt = "disabled";
+      prompt = "enabled";
+      spinner = "enabled";
+    };
+  };
+
+  xdg.configFile."gh/config.yml".force = true;
 
   home.packages = [
     pkgs.git
