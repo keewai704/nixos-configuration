e9bca969099e38ceddb26f0b5cfeb724c5bd4edb
KY <249657796+keewai704@users.noreply.github.com>
2026-09-17 15:08:36 +0900

    Fix OpenAI-only web search routing configuration

--- home/keewai/shared/pi.nix
+++ home/keewai/shared/pi.nix
@@ -14,7 +14,6 @@ let
     searchRouting = {
       providers = [ "openai" ];
       useCurrentModel = true;
-      fallbackOn = [ ];
     };
     workflow = "none";
     allowBrowserCookies = false;
